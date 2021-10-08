88157958533afb81d76c8d0aa59a74108184b1e0
John MacFarlane <jgm@berkeley.edu>
2021-10-08 09:39:00 -0700

    update.hs: add text dependency

--- update.hs
+++ update.hs
@@ -1,5 +1,5 @@
 #!/usr/bin/env stack
--- stack --resolver lts-18.10 script --package aeson --package megaparsec --package bytestring --package pretty-show --package pretty
+-- stack --resolver lts-18.10 script --package text --package aeson --package megaparsec --package bytestring --package pretty-show --package pretty
 {-# LANGUAGE OverloadedStrings #-}
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
